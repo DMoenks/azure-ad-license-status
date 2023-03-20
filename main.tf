e533a4a64c26a9417048e3e5637f51159dc44728
DMoenks <dominik.moenks@gmail.com>
2023-03-20 07:08:20 +0100

    Updated Terraform with recent findings

--- main.tf
+++ main.tf
@@ -124,146 +124,6 @@ provider "azurerm" {
 }
 #endregion
 
-#region: 'azurerm' configuration
-data "azurerm_resource_group" "automation_account_resource_group" {
-  provider = azurerm.automation_account
-  name     = var.automation_account_resource_group_name
-}
-
-data "azurerm_automation_account" "automation_account" {
-  provider            = azurerm.automation_account
-  resource_group_name = var.automation_account_resource_group_name
-  name                = var.automation_account_name
-}
-
-resource "azurerm_automation_module" "automation_module_az_accounts" {
-  name                    = "Az.Accounts"
-  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/Az.Accounts/2.12.1"
-  }
-}
-
-resource "azurerm_automation_module" "automation_module_az_keyvault" {
-  name                    = "Az.KeyVault"
-  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/Az.KeyVault/4.9.2"
-  }
-}
-
-resource "azurerm_automation_module" "automation_module_exchangeonlinemanagement" {
-  name                    = "ExchangeOnlineManagement"
-  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/ExchangeOnlineManagement/3.1.0"
-  }
-}
-
-resource "azurerm_automation_module" "automation_module_microsoft_graph_authentication" {
-  name                    = "Microsoft.Graph.Authentication"
-  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Authentication/1.23.0"
-  }
-}
-
-resource "azurerm_automation_runbook" "automation_runbook_script" {
-  provider                = azurerm.automation_account
-  name                    = "Get-AzureADLicenseStatus"
-  location                = data.azurerm_resource_group.automation_account_resource_group.location
-  resource_group_name     = data.azurerm_automation_account.automation_account.resource_group_name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  runbook_type            = "PowerShell"
-  log_progress            = true
-  log_verbose             = true
-  publish_content_link {
-    uri = "https://raw.githubusercontent.com/DMoenks/azure-ad-license-status/main/azure-ad-license-status.psm1"
-  }
-}
-
-resource "azurerm_automation_runbook" "automation_runbook_script_runner" {
-  provider                = azurerm.automation_account
-  name                    = "Run-AzureADLicenseStatus"
-  location                = data.azurerm_resource_group.automation_account_resource_group.location
-  resource_group_name     = data.azurerm_automation_account.automation_account.resource_group_name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
-  runbook_type            = "PowerShell"
-  log_progress            = true
-  log_verbose             = true
-  content                 = "Get-AzureADLicenseStatus -DirectoryID '${var.tenant_id}' -ApplicationID '${azuread_application.application.id}' -SubscriptionID '${var.key_vault_subscription_id}' -KeyVaultName '${var.key_vault_name}' -CertificateName '${azurerm_key_vault_certificate.key_vault_certificate.name}' -SenderAddress 'sender@example.com' -RecipientAddresses_normal @('recipient@example.com') -AdvancedCheckups"
-}
-
-data "azurerm_resource_group" "key_vault_resource_group" {
-  provider = azurerm.key_vault
-  name     = var.key_vault_resource_group_name
-}
-
-resource "azurerm_key_vault" "key_vault" {
-  provider                  = azurerm.key_vault
-  resource_group_name       = data.azurerm_resource_group.key_vault_resource_group.name
-  name                      = var.solution_name
-  location                  = data.azurerm_resource_group.key_vault_resource_group.location
-  sku_name                  = "standard"
-  tenant_id                 = var.tenant_id
-  enable_rbac_authorization = true
-  access_policy             = []
-}
-
-resource "azurerm_key_vault_certificate" "key_vault_certificate" {
-  provider     = azurerm.key_vault
-  name         = var.solution_name
-  key_vault_id = azurerm_key_vault.key_vault.id
-  certificate_policy {
-    issuer_parameters {
-      name = "Self"
-    }
-    key_properties {
-      exportable = true
-      key_size   = 2048
-      key_type   = "RSA"
-      reuse_key  = true
-    }
-    lifetime_action {
-      action {
-        action_type = "AutoRenew"
-      }
-      trigger {
-        days_before_expiry = 30
-      }
-    }
-    secret_properties {
-      content_type = "application/x-pkcs12"
-    }
-    x509_certificate_properties {
-      extended_key_usage = ["1.3.6.1.5.5.7.3.2"]
-      key_usage = [
-        "cRLSign",
-        "dataEncipherment",
-        "digitalSignature",
-        "keyAgreement",
-        "keyCertSign",
-        "keyEncipherment",
-      ]
-      subject            = "CN=${var.solution_name}"
-      validity_in_months = 12
-    }
-  }
-}
-
-resource "azurerm_role_assignment" "role_assignment" {
-  provider = azurerm.key_vault
-  scope = azurerm_key_vault.key_vault.id
-  role_definition_id = "4633458b-17de-408a-b874-0445c86b69e6"
-  principal_id = data.azurerm_automation_account.automation_account.id
-  skip_service_principal_aad_check = true
-}
-#endregion
-
 #region: 'azuread' configuration
 resource "azuread_application" "application" {
   display_name = var.solution_name
@@ -315,5 +175,136 @@ resource "azuread_directory_role_assignment" "directory_role_assignment" {
   # Global Reader
   role_id             = "f2ef992c-3afb-46b9-b7cf-a126ee74c451"
   principal_object_id = azuread_application.application.object_id
+}
+#endregion
+
+#region: 'azurerm' configuration
+data "azurerm_resource_group" "automation_account_resource_group" {
+  provider = azurerm.automation_account
+  name     = var.automation_account_resource_group_name
+}
+
+data "azurerm_automation_account" "automation_account" {
+  provider            = azurerm.automation_account
+  resource_group_name = var.automation_account_resource_group_name
+  name                = var.automation_account_name
+}
+
+resource "azurerm_automation_module" "automation_module_az_accounts" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "Az.Accounts"
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/Az.Accounts/2.12.1"
+  }
+}
+
+resource "azurerm_automation_module" "automation_module_az_keyvault" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "Az.KeyVault"
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/Az.KeyVault/4.9.2"
+  }
+}
+
+resource "azurerm_automation_module" "automation_module_exchangeonlinemanagement" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "ExchangeOnlineManagement"
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/ExchangeOnlineManagement/3.1.0"
+  }
+}
+
+resource "azurerm_automation_module" "automation_module_microsoft_graph_authentication" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_resource_group.automation_account_resource_group.name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "Microsoft.Graph.Authentication"
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/Microsoft.Graph.Authentication/1.23.0"
+  }
+}
+
+resource "azurerm_automation_runbook" "automation_runbook_script" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_automation_account.automation_account.resource_group_name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "Get-AzureADLicenseStatus"
+  location                = data.azurerm_resource_group.automation_account_resource_group.location
+  runbook_type            = "PowerShell"
+  log_progress            = true
+  log_verbose             = true
+  publish_content_link {
+    uri = "https://raw.githubusercontent.com/DMoenks/azure-ad-license-status/main/azure-ad-license-status.psm1"
+  }
+}
+
+resource "azurerm_automation_runbook" "automation_runbook_script_runner" {
+  provider                = azurerm.automation_account
+  resource_group_name     = data.azurerm_automation_account.automation_account.resource_group_name
+  automation_account_name = data.azurerm_automation_account.automation_account.name
+  name                    = "Run-AzureADLicenseStatus"
+  location                = data.azurerm_resource_group.automation_account_resource_group.location
+  runbook_type            = "PowerShell"
+  log_progress            = true
+  log_verbose             = true
+  content                 = "Get-AzureADLicenseStatus -DirectoryID '${var.tenant_id}' -ApplicationID '${azuread_application.application.id}' -SubscriptionID '${var.key_vault_subscription_id}' -KeyVaultName '${var.key_vault_name}' -CertificateName '${azurerm_key_vault_certificate.key_vault_certificate.name}' -SenderAddress 'sender@example.com' -RecipientAddresses_normal @('recipient@example.com') -AdvancedCheckups"
+}
+
+data "azurerm_resource_group" "key_vault_resource_group" {
+  provider = azurerm.key_vault
+  name     = var.key_vault_resource_group_name
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  provider                  = azurerm.key_vault
+  resource_group_name       = data.azurerm_resource_group.key_vault_resource_group.name
+  name                      = var.solution_name
+  location                  = data.azurerm_resource_group.key_vault_resource_group.location
+  sku_name                  = "standard"
+  tenant_id                 = var.tenant_id
+  enable_rbac_authorization = true
+  access_policy             = []
+}
+
+resource "azurerm_key_vault_certificate" "key_vault_certificate" {
+  provider     = azurerm.key_vault
+  name         = var.solution_name
+  key_vault_id = azurerm_key_vault.key_vault.id
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+    key_properties {
+      exportable = false
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = false
+    }
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+    x509_certificate_properties {
+      extended_key_usage = ["1.3.6.1.5.5.7.3.2"]
+      key_usage = [
+        "digitalSignature"
+      ]
+      subject            = "CN=${var.solution_name}"
+      validity_in_months = 12
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "role_assignment" {
+  provider                         = azurerm.key_vault
+  scope                            = azurerm_key_vault.key_vault.id
+  role_definition_id               = "4633458b-17de-408a-b874-0445c86b69e6"
+  principal_id                     = data.azurerm_automation_account.automation_account.id
+  skip_service_principal_aad_check = true
 }
 #endregion
