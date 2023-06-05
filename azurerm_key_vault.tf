f854792ec383f10da99550853926e005dd4a0948
Dominik Mönks <DMoenks@users.noreply.github.com>
2023-06-05 20:26:40 +0200

    Revert "Updating a later feature branch with an intermediate one"

--- azurerm_key_vault.tf
+++ azurerm_key_vault.tf
@@ -17,18 +17,16 @@ data "azurerm_resource_group" "resource_group_key_vault" {
 }
 
 resource "azurerm_key_vault" "key_vault" {
-  provider                   = azurerm.key_vault
-  resource_group_name        = data.azurerm_resource_group.resource_group_key_vault.name
-  name                       = "kv-${var.solution_name}"
-  location                   = data.azurerm_resource_group.resource_group_key_vault.location
-  sku_name                   = "standard"
-  tenant_id                  = var.tenant_id
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = true
-  enable_rbac_authorization  = true
-  access_policy              = []
+  provider                  = azurerm.key_vault
+  resource_group_name       = data.azurerm_resource_group.resource_group_key_vault.name
+  name                      = "kv-${var.solution_name}"
+  location                  = data.azurerm_resource_group.resource_group_key_vault.location
+  sku_name                  = "standard"
+  tenant_id                 = var.tenant_id
+  enable_rbac_authorization = true
+  access_policy             = []
+  purge_protection_enabled  = true
   network_acls {
-    # The key vault needs to be accessed by the automation account (which isn't one of the trusted Azure services) for certificate retrieval, therefore requiring public access
     default_action = "Allow"
     bypass         = "AzureServices"
   }
