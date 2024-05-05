0d807825c73a54c19c008b299a92ad9a89ce1d52
DMoenks <dominik.moenks@gmail.com>
2024-05-05 22:23:48 +0200

    Updated Terraform configuration

--- azuread_application.tf
+++ azuread_application.tf
@@ -9,7 +9,7 @@ data "azuread_client_config" "client_config_azuread_application" {}
 resource "azuread_application" "application" {
   display_name = var.solution_name
   required_resource_access {
-    resource_app_id = data.azuread_service_principal.service_principal_graph.application_id
+    resource_app_id = data.azuread_service_principal.service_principal_graph.client_id
     resource_access {
       id   = data.azuread_service_principal.service_principal_graph.app_role_ids["DeviceManagementManagedDevices.Read.All"]
       type = "Role"
@@ -32,7 +32,7 @@ resource "azuread_application" "application" {
     }
   }
   required_resource_access {
-    resource_app_id = data.azuread_service_principal.service_principal_exchange_online.application_id
+    resource_app_id = data.azuread_service_principal.service_principal_exchange_online.client_id
     resource_access {
       id   = data.azuread_service_principal.service_principal_exchange_online.app_role_ids["Exchange.ManageAsApp"]
       type = "Role"
@@ -77,26 +77,26 @@ resource "azuread_app_role_assignment" "app_role_assignment_exchange_online_exch
 }
 
 resource "azuread_application_certificate" "application_certificate" {
-  application_object_id = azuread_application.application.object_id
-  encoding              = "hex"
-  type                  = "AsymmetricX509Cert"
-  value                 = azurerm_key_vault_certificate.key_vault_certificate.certificate_data
-  start_date            = azurerm_key_vault_certificate.key_vault_certificate.certificate_attribute[0].not_before
-  end_date              = azurerm_key_vault_certificate.key_vault_certificate.certificate_attribute[0].expires
+  application_id = azuread_application.application.client_id
+  encoding       = "hex"
+  type           = "AsymmetricX509Cert"
+  value          = azurerm_key_vault_certificate.key_vault_certificate.certificate_data
+  start_date     = azurerm_key_vault_certificate.key_vault_certificate.certificate_attribute[0].not_before
+  end_date       = azurerm_key_vault_certificate.key_vault_certificate.certificate_attribute[0].expires
 }
 
 data "azuread_service_principal" "service_principal_graph" {
   # Provider: Microsoft Graph
-  application_id = "00000003-0000-0000-c000-000000000000"
+  client_id = "00000003-0000-0000-c000-000000000000"
 }
 
 data "azuread_service_principal" "service_principal_exchange_online" {
   # Provider: Office 365 Exchange Online
-  application_id = "00000002-0000-0ff1-ce00-000000000000"
+  client_id = "00000002-0000-0ff1-ce00-000000000000"
 }
 
 resource "azuread_service_principal" "service_principal_azure_ad_license_status" {
-  application_id = azuread_application.application.application_id
+  client_id = azuread_application.application.client_id
   owners = [
     data.azuread_client_config.client_config_azuread_application.object_id
   ]
